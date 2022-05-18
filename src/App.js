690d526271d9063036f9a3b57eb84370fb5902ba
chanatip payomyam <chanatip-ninenine@gmail.com>
2022-05-19 03:56:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import liff from "@line/liff";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pictureUrl, setPictureUrl] = useState(logo);
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  };
+
+  const initLine = () => {
+    liff.init(
+      { liffId: "1657145186-RoQ69aKB" },
+      () => {
+        if (liff.isLoggedIn()) {
+          runApp();
+        } else {
+          liff.login();
+        }
+      },
+      (err) => console.error(err)
+    );
+  };
+
+  const runApp = () => {
+    const idToken = liff.getIDToken();
+    setIdToken(idToken);
+    liff
+      .getProfile()
+      .then((profile) => {
+        console.log(profile);
+        setDisplayName(profile.displayName);
+        setPictureUrl(profile.pictureUrl);
+        setStatusMessage(profile.statusMessage);
+        setUserId(profile.userId);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    initLine();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ textalign: "center" }}>
+          <h1>React with LINE Login</h1>
+          <hr />
+          <img src={pictureUrl} width="300px" height="300px" />
+          <p
+            style={{ textalign: "left", marginleft: "20%", marginright: "20%" }}
+          >
+            <b>id token: </b> {idToken}
+          </p>
+          <p
+            style={{ textalign: "left", marginleft: "20%", marginright: "20%" }}
+          >
+            <b>display name: </b> {displayName}
+          </p>
+          <p
+            style={{ textalign: "left", marginleft: "20%", marginright: "20%" }}
+          >
+            <b>status message: </b> {statusMessage}
+          </p>
+          <p
+            style={{ textalign: "left", marginleft: "20%", marginright: "20%" }}
+          >
+            <b>user id: </b> {userId}
+          </p>
+
+          <button
+            onClick={() => logout()}
+            style={{ width: "100%", height: "30px" }}
+          >
+            Logout
+          </button>
+        </div>
       </header>
     </div>
   );
